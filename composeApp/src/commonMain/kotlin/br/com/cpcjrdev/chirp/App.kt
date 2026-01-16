832f823d5e92500fda0d294060edfdeb845d3f8e
Cyro Paulino da Costa Jr <cyropcjr@gmail.com>
2026-01-16 00:02:20 -0300

    feat: Add navigation graph for authentication flow (#15)
    - Introduces a navigation graph for the authentication flow using JetBrains Compose Navigation.
    - Adds AuthGraph and AuthGraphRoutes, updates the app entry point to use NavigationRoot, and wires up navigation between registration and registration success screens.
    - Also updates dependencies, Gradle, and Android build settings, and refines URL constants.

--- composeApp/src/commonMain/kotlin/br/com/cpcjrdev/chirp/App.kt
+++ composeApp/src/commonMain/kotlin/br/com/cpcjrdev/chirp/App.kt
@@ -1,7 +1,7 @@
 package br.com.cpcjrdev.chirp
 
 import androidx.compose.runtime.Composable
-import br.com.cpcjrdev.auth.presentantion.register.RegisterRoot
+import br.com.cpcjrdev.chirp.navigation.NavigationRoot
 import br.com.cpcjrdev.core.designsystem.theme.ChirpTheme
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
@@ -9,6 +9,6 @@ import org.jetbrains.compose.ui.tooling.preview.Preview
 @Preview
 fun App() {
     ChirpTheme {
-        RegisterRoot(onRegisterSuccess = {})
+        NavigationRoot()
     }
 }
