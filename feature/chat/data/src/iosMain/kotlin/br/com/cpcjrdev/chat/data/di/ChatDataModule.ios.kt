6a026d63cdea47b228cbc5d7f1244992a46ac59a
Cyro P Costa Jr <cyropcjr@gmail.com>
2026-05-27 17:29:29 -0300

    update: update libs version catalog, add actual observer check connection througth all platforms supported.

---
+++ feature/chat/data/src/iosMain/kotlin/br/com/cpcjrdev/chat/data/di/ChatDataModule.ios.kt
@@ -0,0 +1,13 @@
+package br.com.cpcjrdev.chat.data.di
+
+import br.com.cpcjrdev.chat.data.lifecycle.AppLifecycleObserver
+import br.com.cpcjrdev.chat.data.network.ConnectivityObserver
+import br.com.cpcjrdev.chat.database.DatabaseFactory
+import org.koin.core.module.dsl.singleOf
+import org.koin.dsl.module
+
+actual val platformChatDataModule = module {
+    single { DatabaseFactory() }
+    singleOf(::AppLifecycleObserver)
+    singleOf(::ConnectivityObserver)
+}
